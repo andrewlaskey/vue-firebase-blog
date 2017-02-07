217349d4de5f629ab03a79740f6d56fe4f09528e
Andrew Laskey <andrew@idiom.co>
2017-02-06 20:42:13 -0800

    Get site data from firebase

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="about">
-		<h1>Hello</h1>
-		<p>This is something else</p>
+		<h1>{{ headline }}</h1>
+		<p>{{ description }}</p>
 	</div>
 </template>
 
@@ -11,7 +11,23 @@ import firebase from 'firebase'
 export default {
 	name: 'about',
 	data () {
-		return {}
+		return {
+			title: 'Andrew Laskey',
+			headline: 'I make web things',
+			description: 'This is a description'
+		}
+	},
+	created: function () {
+		const siteRef = firebase.database().ref('site')
+
+		siteRef.once('value')
+			.then(snapshot => {
+				let siteData = snapshot.val()
+
+				this.title = siteData.title
+				this.headline = siteData.headline
+				this.description = siteData.description
+			});
 	}
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@
 // so you don't have to do: import Vue from 'vue/dist/vue'
 // This is done with the browser options. For the config, see package.json
 import Vue from 'vue'
+import {CONFIG} from '../private.config.js'
+import firebase from 'firebase'
 import App from './App.vue'
+
+const fbApp = firebase.initializeApp(CONFIG.FIREBASE)
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
